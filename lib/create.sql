8a21c1e8f0f1fdb4ed1400c91f9451acd0c660e2
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 09:53:38 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding_goal INTEGER,
+  funding_goal REAL,
   start_date DATE,
   end_date DATE
 );
@@ -11,4 +11,11 @@ CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount REAL,
+  user_id INTEGER,
+  project_id INTEGER
 );
